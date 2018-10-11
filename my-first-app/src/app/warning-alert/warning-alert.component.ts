06e55e6e56e85be2c0c9bf532c8de4e130257761
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-10 22:31:02 -0400

    Assignment completed
    Assignment completed

---
+++ my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-warning-alert',
+    template: `
+        <div class="alert alert-danger" role="alert">
+            A simple danger alert—check it out!
+        </div>
+    `
+})
+export class WarningAlertComponent {
+
+}
