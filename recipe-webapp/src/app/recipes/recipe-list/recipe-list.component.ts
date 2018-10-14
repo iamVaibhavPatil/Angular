fdd4d41b03338edb99b8e38bd71b6331b4a1621c
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-14 17:41:40 -0400

    Recipe App Show Details of the selected recipe
    Recipe App Show Details of the selected recipe

--- recipe-webapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-webapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,14 +8,21 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is simply a test', 'https://images.media-allrecipes.com/userphotos/560x315/5565969.jpg'),
-    new Recipe('A test Recipe', 'This is simply a test', 'https://images.media-allrecipes.com/userphotos/560x315/5565969.jpg')
+    new Recipe('A test Recipe1', 'This is simply a test', 'https://images.media-allrecipes.com/userphotos/560x315/5565969.jpg'),
+    new Recipe('A test Recipe2', 'This is simply a test', 'https://images.media-allrecipes.com/userphotos/560x315/5565969.jpg'),
+    new Recipe('A test Recipe3', 'This is simply a test', 'https://images.media-allrecipes.com/userphotos/560x315/5565969.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(this.recipes.find(r => r.name === recipe.name));
   }
 
 }
