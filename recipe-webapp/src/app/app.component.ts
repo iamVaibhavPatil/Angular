8dadb22e5644199ba6abb18a96437cf123112d48
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-14 17:06:41 -0400

    Added Navigation to recipe project
    Added Navigation to recipe project

--- recipe-webapp/src/app/app.component.ts
+++ recipe-webapp/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  selectedNavigation = 'recipes';
+
+  onNavigate(feature: string) {
+    this.selectedNavigation = feature;
+  }
 }
