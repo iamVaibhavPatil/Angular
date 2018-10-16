1fc557a60a92c1bdbb7e81ca61dc7abb4ea5815a
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-16 01:46:21 -0400

    Shopping List Service to add ingredients from Recipe
    Shopping List Service to add ingredients from Recipe

--- recipe-webapp/src/app/recipes/recipe.service.ts
+++ recipe-webapp/src/app/recipes/recipe.service.ts
@@ -1,19 +1,42 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
+    constructor(private shoppingListService: ShoppingListService) {}
+
     private recipes: Recipe[] = [
-        new Recipe('A test Recipe1', 'This is simply a test', 'https://images.media-allrecipes.com/userphotos/560x315/5565969.jpg'),
-        new Recipe('A test Recipe2', 'This is simply a test', 'https://images.media-allrecipes.com/userphotos/560x315/5565969.jpg'),
-        new Recipe('A test Recipe3', 'This is simply a test', 'https://images.media-allrecipes.com/userphotos/560x315/5565969.jpg')
-      ];
+        new Recipe(
+            'Chicken Burger',
+            'Chicken Burger is yummy',
+            'http://robertsboxedmeats.ca/wp-content/uploads/2015/01/120518_CrispyChickenBurger_hr.jpg',
+            [
+                new Ingredient('Chick Meat', 1),
+                new Ingredient('Tommatoes', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+            new Recipe(
+                'Italian Pizza',
+                'Italian Pizza is tasty',
+                'https://cdn.yemek.com/mncrop/940/625/uploads/2016/08/makarna-tabanli-pizza.jpg',
+                [
+                    new Ingredient('Capsicum', 3),
+                    new Ingredient('Onion', 1),
+                    new Ingredient('Cheese', 2)
+            ])
+        ];
 
     getRecipes() {
         return this.recipes.slice();
     }
 
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    }
 }
