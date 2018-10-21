ae72a5ea146712777062e9037a8298492186e208
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-21 14:23:10 -0400

    Can Deactivate to move way from page.
    Can Deactivate to move way from page.

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { UsersComponent } from './users/users.component';
 import { UserComponent } from './users/user/user.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-gaurd.service';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,7 +18,7 @@ const appRoutes: Routes = [
         component: ServersComponent,
         children: [
             { path: ':id', component: ServerComponent },
-            { path: ':id/edit', component: EditServerComponent }
+            { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
         ]
     },
     { path: 'users', component: UsersComponent,
