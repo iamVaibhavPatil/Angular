9d97e7bd8ffdceacbcba6aaed891402057dc9e60
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-22 19:04:59 -0400

    Set Value and Patch Value to Patch the parts of the form.
    Set Value and Patch Value to Patch the parts of the form.

--- forms-template-driven/src/app/app.component.ts
+++ forms-template-driven/src/app/app.component.ts
@@ -15,7 +15,25 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-  }
+
+    // Set the value - To set the whole form
+    /*this.signUpForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    });*/
+
+    // PatchValue to patch parts of the form
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+}
 
   /*onSubmit(form: NgForm) {
     console.log(form);
