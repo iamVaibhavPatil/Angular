2db43539c3e15476a0fc959b0bc6ddaefea49e77
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-30 16:20:07 -0400

    Header redirect sign in
    Header redirect sign in

--- recipe-webapp/src/app/header/header.component.ts
+++ recipe-webapp/src/app/header/header.component.ts
@@ -3,6 +3,7 @@ import { DataStorageService } from '../shared/data-storage.service';
 import { Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +12,8 @@ import { AuthService } from '../auth/auth.service';
 export class HeaderComponent implements OnInit {
 
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              private router: Router) { }
 
   onSaveData() {
     this.dataStorageService.storeRecipes().subscribe((response: Response) => {
@@ -25,6 +27,7 @@ export class HeaderComponent implements OnInit {
 
   onLogOut() {
     this.authService.logout();
+    this.router.navigate(['/signin']);
   }
 
   ngOnInit() {
