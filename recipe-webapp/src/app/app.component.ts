da423d30292c75851cad1736556b8c1a7cea4af2
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-30 14:58:16 -0400

    Sign Up and Sign In
    Sign Up and Sign In

--- recipe-webapp/src/app/app.component.ts
+++ recipe-webapp/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCfylQiDAcc6K7yMa0Mird85T7KLiQnLBo',
+      authDomain: 'ng-recipe-boot-44ca4.firebaseapp.com'
+    });
+  }
 }
---
+++ recipe-webapp/src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+
+    signupUser(email: string, password: string) {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    signinUser(email: string, password: string) {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+}
