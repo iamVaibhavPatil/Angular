13bfed69b01fd59fd8d6f573ed13d1ce36b1e1ab
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-30 15:47:04 -0400

    Authorizing with tokens
    Authorizing with tokens

--- recipe-webapp/src/app/auth/auth.service.ts
+++ recipe-webapp/src/app/auth/auth.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthService {
 
+    token: string;
+
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
         .then(response => {
@@ -17,10 +19,15 @@ export class AuthService {
     signinUser(email: string, password: string) {
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(response => {
-            console.log(response);
+            firebase.auth().currentUser.getIdToken().then((token: string) => this.token = token);
         })
         .catch(error => {
             console.log(error);
         });
+    }
+
+    getToken() {
+        firebase.auth().currentUser.getIdToken().then((token: string) => this.token = token);
+        return this.token;
     }
 }
