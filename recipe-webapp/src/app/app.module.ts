a6d5f01a8de2b86ffb760f4d809964a982aae75f
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-10-31 19:04:57 -0400

    Moving Providers to Core Module
    Moving Providers to Core Module

--- recipe-webapp/src/app/app.module.ts
+++ recipe-webapp/src/app/app.module.ts
@@ -2,13 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RoutingModule } from './routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
 import { HttpModule } from '@angular/http';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -27,7 +22,6 @@ import { CoreModule } from './core/core.module';
     SharedModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- recipe-webapp/src/app/core/core.module.ts
+++ recipe-webapp/src/app/core/core.module.ts
@@ -3,6 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { RoutingModule } from '../routing.module';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { AuthGuard } from '../auth/auth-guard.service';
 
 @NgModule({
     declarations: [
@@ -15,6 +20,13 @@ import { RoutingModule } from '../routing.module';
     ],
     exports: [
         HeaderComponent
-    ]
+    ],
+    providers: [
+        ShoppingListService,
+        RecipeService,
+        DataStorageService,
+        AuthService,
+        AuthGuard
+    ],
 })
 export class CoreModule {}
